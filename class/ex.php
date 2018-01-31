612b5be282fd72059d33f30d276419aa8af21c5c
Victor <victor.h.s.reis@gmail.com>
2018-01-31 21:57:28 -0200

    Classes simpes

---
+++ class/ex.php
@@ -0,0 +1,18 @@
+<?php
+class Pessoa {
+
+	public $nome; // Atributo
+
+	public function falar()
+	{ // metodo publico
+		return "O meu nome é ".$this->nome;
+	}
+	
+
+}
+
+$user = new Pessoa();
+$user->nome="Victor";
+echo $user->falar();
+
+?>
